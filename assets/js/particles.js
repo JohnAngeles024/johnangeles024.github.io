6bf7f69e2fffeb758bfd499942709eb60b55b6ee
John <johnangeles024@gmail.com>
2024-04-30 16:28:18 +0800

    deleted unnecessary css files and added a loader

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -111,17 +111,6 @@ particlesJS("particles-js", {
 "retina_detect": true
 });
 
-
-/* ---- stats.js config ---- */
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
 update = function() {
 stats.begin();
 stats.end();
